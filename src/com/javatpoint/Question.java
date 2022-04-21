476228c5385e49ca87bb45d6e9709392f7b3e98e
AidinDeveloper <aidin.masroor@gmail.com>
2022-04-21 15:29:13 +0400

    Constructor Injection with Map Example

--- src/com/javatpoint/Question.java
+++ src/com/javatpoint/Question.java
@@ -1,14 +1,19 @@
 package com.javatpoint;
 
 import java.util.Iterator;
-import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.Map.Entry;
 
 public class Question {
-	int id;
-	String name;
-	List<Answer> answers;
+	private int id;
+	private String name;
+	private Map<String, String> answers;
 
-	public Question(int id, String name, List<Answer> answers) {
+	public Question() {
+	}
+
+	public Question(int id, String name, Map<String, String> answers) {
 		super();
 		this.id = id;
 		this.name = name;
@@ -16,11 +21,14 @@ public class Question {
 	}
 
 	public void displayInfo() {
-		System.out.println(id + " " + name);
-		System.out.println("answers are:");
-		Iterator<Answer> itr = answers.iterator();
+		System.out.println("question id:" + id);
+		System.out.println("question name:" + name);
+		System.out.println("Answers....");
+		Set<Entry<String, String>> set = answers.entrySet();
+		Iterator<Entry<String, String>> itr = set.iterator();
 		while (itr.hasNext()) {
-			System.out.println(itr.next());
+			Entry<String, String> entry = itr.next();
+			System.out.println("Answer:" + entry.getKey() + " Posted By:" + entry.getValue());
 		}
 	}
 }
