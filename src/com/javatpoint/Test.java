6956148369261216ddcc969d0ce81756aba679d7
AidinDeveloper <aidin.masroor@gmail.com>
2022-04-21 18:58:59 +0430

    Dependency Injection with Factory Method in Spring- Type 1

--- src/com/javatpoint/Test.java
+++ src/com/javatpoint/Test.java
@@ -1,18 +1,12 @@
 package com.javatpoint;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 
 public class Test {
 	public static void main(String[] args) {
-		Resource r = new ClassPathResource("applicationContext.xml");
-		BeanFactory factory = new XmlBeanFactory(r);
-
-		Question q = (Question) factory.getBean("q");
-		q.displayInfo();
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		A a = (A) context.getBean("a");
+		a.msg();
 	}
 }
