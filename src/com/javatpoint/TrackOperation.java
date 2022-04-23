76ffb0dc5f28f80f03ac25cb311b5d879d59731a
AidinDeveloper <aidin.masroor@gmail.com>
2022-04-23 14:41:11 +0430

    Spring AOP AspectJ Annotation Example, @After

--- src/com/javatpoint/TrackOperation.java
+++ src/com/javatpoint/TrackOperation.java
@@ -1,24 +1,19 @@
 package com.javatpoint;
 
-import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.JoinPoint;  
+import org.aspectj.lang.annotation.Aspect;  
+import org.aspectj.lang.annotation.After;  
+import org.aspectj.lang.annotation.Pointcut;  
 
-@Aspect
-public class TrackOperation {
-	//@Pointcut("execution(* Operation.*(int,*))")
-	//@Pointcut("execution(* Operation.*(*,String*))")
-	//@Pointcut("execution(* Operation.*(*))")
-	@Pointcut("execution(* Operation.*(..))")
-	public void k() {
-		System.out.println("aaaa");
-	}// pointcut name
-
-	@Before("k()") // applying pointcut on before advice
-	public void myadvice(JoinPoint jp)// it is advice (before advice)
-	{
-		System.out.println("additional concern");
-		// System.out.println("Method Signature: " + jp.getSignature());
-	}
+@Aspect  
+public class TrackOperation{  
+    @Pointcut("execution(* Operation.*(..))")  
+    public void k(){}//pointcut name  
+      
+    @After("k()")//applying pointcut on after advice  
+    public void myadvice(JoinPoint jp)//it is advice (after advice)  
+    {  
+        System.out.println("additional concern");  
+        //System.out.println("Method Signature: "  + jp.getSignature());  
+    }  
 }
