12ffc94b28d7f5f1a24011bb0de3d0ccd9f10e23
AidinDeveloper <aidin.masroor@gmail.com>
2022-04-25 08:17:36 +0430

    Spring AOP AspectJ Xml Configuration Example- Before

--- src/com/javatpoint/Test.java
+++ src/com/javatpoint/Test.java
@@ -6,19 +6,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Test {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-		Operation op = (Operation) context.getBean("opBean");
-		System.out.println("calling validate...");
-		try {
-			op.validate(19);
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-		System.out.println("calling validate again...");
-
-		try {
-			op.validate(11);
-		} catch (Exception e) {
-			System.out.println(e);
-		}
+		Operation e = (Operation) context.getBean("opBean");
+		System.out.println("calling msg...");
+		e.msg();
+		System.out.println("calling m...");
+		e.m();
+		System.out.println("calling k...");
+		e.k();
 	}
 }
--- src/com/javatpoint/TrackOperation.java
+++ src/com/javatpoint/TrackOperation.java
@@ -1,22 +1,11 @@
 package com.javatpoint;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Aspect;
 
-@Aspect
-public class TrackOperation {
-	@AfterThrowing(pointcut = "execution(* Operation.*(..))", throwing = "error")
-
-	public void myadvice(JoinPoint jp, Throwable error)// it is advice
+public class TrackOperation {// Aspect Class
+	public void myadvice(JoinPoint jp)// Advice Method
 	{
 		System.out.println("additional concern");
 		System.out.println("Method Signature: " + jp.getSignature());
-		System.out.println("Exception is: " + error);
-		
-		Object []inputs=jp.getArgs();
-		for (Object object : inputs) {
-			System.out.println("Input: "+object);
-		}
 	}
 }
