bb22fb9ceca4e17e8814faef6e97ad1effc512e4
AidinDeveloper <aidin.masroor@gmail.com>
2022-04-25 08:25:03 +0430

    Spring AOP AspectJ Xml Configuration Example- After

--- src/com/javatpoint/TrackOperation.java
+++ src/com/javatpoint/TrackOperation.java
@@ -3,9 +3,15 @@ package com.javatpoint;
 import org.aspectj.lang.JoinPoint;
 
 public class TrackOperation {// Aspect Class
-	public void myadvice(JoinPoint jp)// Advice Method
+	public void myadvice(JoinPoint jp)// Advice Method Before
 	{
-		System.out.println("additional concern");
+		System.out.println("additional concern, Before");
+		System.out.println("Method Signature: " + jp.getSignature());
+	}
+	
+	public void myadvice2(JoinPoint jp)// Advice Method After
+	{
+		System.out.println("additional concern, After");
 		System.out.println("Method Signature: " + jp.getSignature());
 	}
 }
