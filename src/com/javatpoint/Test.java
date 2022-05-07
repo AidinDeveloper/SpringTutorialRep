34da0f9d504655f041ac16770f07508156399d05
Aidin Masroor1 <Aidin.masroor@gmail.com>
2022-05-07 19:59:24 +0430

    ResultSetExtractor

--- src/com/javatpoint/Test.java
+++ src/com/javatpoint/Test.java
@@ -1,5 +1,7 @@
 package com.javatpoint;
 
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -8,17 +10,8 @@ public class Test {
 	public static void main(String[] args) {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
 		EmployeeDao dao = (EmployeeDao) ctx.getBean("edao");
-		// int status = dao.saveEmployee(new Employee(102, "Amit", 35000));
-		// System.out.println(status);
-
-		/*
-		 * int status = dao.updateEmployee(new Employee(102, "Sonoo1", 15000));
-		 * //returns the number of affected rows System.out.println(status);
-		 */
-
-		Employee e = new Employee();
-		e.setId(102);
-		int status = dao.deleteEmployee(e);//returns the number of affected rows System.out.println(status);
-		System.out.println(status);
+		List<Employee> list = dao.getAllEmployees();
+		for (Employee e : list)
+			System.out.println(e);
 	}
 }
