e60855185ab92e3804e7d46211c34237123cdbbb
Aidin Masroor1 <Aidin.masroor@gmail.com>
2022-05-07 20:08:03 +0430

    RowMapper

--- src/com/javatpoint/EmployeeDao.java
+++ src/com/javatpoint/EmployeeDao.java
@@ -2,11 +2,9 @@ package com.javatpoint;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
-import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
 
 public class EmployeeDao {
 	private JdbcTemplate template;
@@ -16,19 +14,15 @@ public class EmployeeDao {
 	}
 
 	public List<Employee> getAllEmployees() {
-		return template.query("select * from employee", new ResultSetExtractor<List<Employee>>() {
-			@Override
-			public List<Employee> extractData(ResultSet rs) throws SQLException, DataAccessException {
+		return template.query("select * from employee", new RowMapper<Employee>() {
 
-				List<Employee> list = new ArrayList<Employee>();
-				while (rs.next()) {
-					Employee e = new Employee();
-					e.setId(rs.getInt("id"));
-					e.setName(rs.getString(2));
-					e.setSalary(rs.getInt(3));
-					list.add(e);
-				}
-				return list;
+			@Override
+			public Employee mapRow(ResultSet rs, int rowNumber) throws SQLException {
+				Employee e=new Employee();
+				e.setId(rs.getInt("id"));
+				e.setName(rs.getString("name"));
+				e.setSalary(rs.getInt("salary"));
+				return e;
 			}
 		});
 	}
