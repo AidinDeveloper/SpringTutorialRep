c66a61855baaec95f9bcead2e504fc23a4869429
Aidin Masroor1 <Aidin.masroor@gmail.com>
2022-05-10 23:41:15 +0430

    Operators in SPEL

--- src/com/javatpoint/Test.java
+++ src/com/javatpoint/Test.java
@@ -1,6 +1,5 @@
 package com.javatpoint;
 
-import org.springframework.expression.Expression;
 import org.springframework.expression.ExpressionParser;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 
@@ -8,10 +7,15 @@ public class Test {
 	public static void main(String[] args) {
 		ExpressionParser parser = new SpelExpressionParser();
 
-		Expression exp = parser.parseExpression("'Hello SPEL'");
-		String message = (String) exp.getValue();
-		System.out.println(message);
-//OR  
-//System.out.println(parser.parseExpression("'Hello SPEL'").getValue());  
+//arithmetic operator  
+		System.out.println(parser.parseExpression("'Welcome SPEL'+'!'").getValue());
+		System.out.println(parser.parseExpression("10 * 10/2").getValue());
+		System.out.println(parser.parseExpression("'Today is: '+ new java.util.Date()").getValue());
+
+//logical operator  
+		System.out.println(parser.parseExpression("true and true").getValue());
+
+//Relational operator  
+		System.out.println(parser.parseExpression("'sonoo'.length()==5").getValue());
 	}
 }
