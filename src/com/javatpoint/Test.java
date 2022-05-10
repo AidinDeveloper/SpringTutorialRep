3fe2b7038cb52475809744850a836525ce868b30
Aidin Masroor1 <Aidin.masroor@gmail.com>
2022-05-10 23:40:12 +0430

    SPEL

--- src/com/javatpoint/Test.java
+++ src/com/javatpoint/Test.java
@@ -1,17 +1,17 @@
 package com.javatpoint;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
+import org.springframework.expression.Expression;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
 
 public class Test {
-
 	public static void main(String[] args) {
-		Resource r = new ClassPathResource("applicationContext.xml");
-		BeanFactory factory = new XmlBeanFactory(r);
-		EmployeeDao dao = (EmployeeDao) factory.getBean("edao");
-		Object o=dao.update(new Employee(22101223, "sonoo", 50000));
-		System.out.println("Done");
+		ExpressionParser parser = new SpelExpressionParser();
+
+		Expression exp = parser.parseExpression("'Hello SPEL'");
+		String message = (String) exp.getValue();
+		System.out.println(message);
+//OR  
+//System.out.println(parser.parseExpression("'Hello SPEL'").getValue());  
 	}
 }
