4d6a41952ea60b1e93d38938b91d4f400a88822f
Aidin Masroor1 <Aidin.masroor@gmail.com>
2022-05-17 16:16:39 +0430

    MVC RequestParam Annotation

--- src/main/java/com/javatpoint/HelloController.java
+++ src/main/java/com/javatpoint/HelloController.java
@@ -1,18 +1,16 @@
 package com.javatpoint;
 
-import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
 	@RequestMapping("/hello")
-	public String display(HttpServletRequest req, Model m) {
-		// read the provided form data
-		String name = req.getParameter("name");
-		String pass = req.getParameter("pass");
+	// read the provided form data
+	public String display(@RequestParam("name") String name, @RequestParam("pass") String pass, Model m) {
 		if (pass.equals("admin")) {
 			String msg = "Hello " + name;
 			// add a message to the model
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -3,7 +3,7 @@
 <body>
 	<form action="hello">
 		UserName : <input type="text" name="name" /> <br>
-		<br> Password : <input type="text" name="pass" /> <br>
+		<br> Password : <input type="text" name="pass2" /> <br>
 		<br> <input type="submit" name="submit">
 	</form>
 </body>
