b718b4377fef5934926902e726d3357057458463
Aidin Masroor1 <Aidin.masroor@gmail.com>
2022-05-20 13:32:44 +0430

    Castor

--- src/com/javatpoint/Client.java
+++ src/com/javatpoint/Client.java
@@ -10,12 +10,12 @@ import org.springframework.oxm.Marshaller;
 public class Client {
 	public static void main(String[] args) throws IOException {
 		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-		Marshaller marshaller = (Marshaller) context.getBean("xstreamMarshallerBean");
+		Marshaller marshaller = (Marshaller) context.getBean("castorMarshallerBean");
 
 		Employee employee = new Employee();
-		employee.setId(10);
-		employee.setName("Aidin Masroor");
-		employee.setSalary(2022);
+		employee.setId(101);
+		employee.setName("Sonoo Jaiswal");
+		employee.setSalary(100000);
 
 		marshaller.marshal(employee, new StreamResult(new FileWriter("employee.xml")));
 
