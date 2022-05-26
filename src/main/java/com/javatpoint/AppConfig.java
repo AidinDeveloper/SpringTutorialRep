098b21cac73cc095bae984f25325aef81fffe894
Aidin Masroor1 <Aidin.masroor@gmail.com>
2022-05-26 11:47:18 +0430

    Spring Security Login-Logout Module Example

--- src/main/java/com/javatpoint/AppConfig.java
+++ src/main/java/com/javatpoint/AppConfig.java
@@ -1,14 +1,22 @@
 package com.javatpoint;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@EnableWebMvc
 @Configuration
-@EnableWebSecurity // (1)
-public class AppConfig extends WebSecurityConfigurerAdapter {
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().authenticated().and().formLogin().and().httpBasic();
+@ComponentScan({ "com.javatpoint.controller.*" })
+public class AppConfig {
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
 	}
 }
--- src/main/java/com/javatpoint/WebSecurityConfig.java
+++ src/main/java/com/javatpoint/WebSecurityConfig.java
@@ -11,18 +11,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebSecurity
 @ComponentScan("com.javatpoint")
-public class WebSecurityConfig implements WebMvcConfigurer {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Bean
-	public UserDetailsService userDetailsService() throws Exception {
+	public UserDetailsService userDetailsService() {
 		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-		manager.createUser(
-				User.withDefaultPasswordEncoder().username("Aidin").password("123456").roles("USER").build());
+		manager.createUser(User.withDefaultPasswordEncoder().username("Aidin").password("1234").roles("ADMIN").build());
 		return manager;
 	}
 
+	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
-		http.antMatcher("/").authorizeRequests().anyRequest().hasRole("ADMIN").and().httpBasic();
+		http.authorizeRequests().anyRequest().hasRole("ADMIN").and().formLogin().and().httpBasic().and().logout()
+				.logoutUrl("/j_spring_security_logout").logoutSuccessUrl("/");
 	}
 }
